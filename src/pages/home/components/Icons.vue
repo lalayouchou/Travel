c80631a7560fe1ab38b47bab4cea628f249fd155
lalayouchou <1436714481@qq.com>
2018-05-09 23:23:09 +0800

    add icon

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,114 @@
+﻿<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+       <div class="icon" v-for="item of page" :key="item.id">
+        <div class="icon-img">
+          <img class="icon-img-content" :src="item.imgUrl" alt="">
+        </div>
+         <p class="icon-text">{{item.desc}}</p>
+       </div>
+      </swiper-slide>
+
+   </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {},
+      iconLists: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点热门景点'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      let pages = []
+      this.iconLists.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/ellipsis.styl'
+  .icons >>> .swiper-slide-active
+    padding-bottom 50%
+    height 0
+  .icons
+    .icon
+      position relative
+      width 25%
+      padding-bottom 25%
+      float left
+      .icon-img
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom .44rem
+        .icon-img-content
+          display block
+          height 100%
+          margin auto
+          box-sizing border-box
+          padding-top .1rem
+      .icon-text
+        position absolute
+        bottom 0
+        left 0
+        right 0
+        height .44rem
+        line-height .44rem
+        text-align center
+        ellipsis()
+</style>
