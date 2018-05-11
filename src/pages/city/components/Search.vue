e08da85e525a9f99c819da934015e4d313a92591
lalayouchou <1436714481@qq.com>
2018-05-12 00:49:47 +0800

    search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,14 +1,68 @@
 <template>
   <div>
     <div class="search">
-      <input type="text" class="search-input" placeholder="输入城市名或拼音">
+      <input
+      type="text"
+      class="search-input"
+      placeholder="输入城市名或拼音"
+      v-model="keyword"
+      />
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li
+        class="search-item border-bottom"
+        v-for="item of searchlist"
+        :key="item.id"
+        >{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      searchlist: [],
+      timer: null
+    }
+  },
+  computed:{
+    hasNoData () {
+      return !this.searchlist.length
+    }
+  },
+  watch: {
+    keyword () {
+      if(this.timer){
+        clearTimeout(this.timer)
+      }
+      
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.name.indexOf(this.keyword) > -1||
+              value.spell.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        return this.searchlist = result
+      },100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -28,4 +82,16 @@ export default {
    font-size .2rem
    padding 0 .1rem
    color #666
+.search-content
+  overflow hidden
+  z-index 1
+  position absolute
+  left 0
+  right 0
+  bottom 0
+  top 1.64rem
+  background #fff
+  .search-item
+    padding-left .3rem
+    line-height .55rem
 </style>
