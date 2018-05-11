b61232a316c791f3c72bb62baff8127907366813
lalayouchou <1436714481@qq.com>
2018-05-11 19:22:22 +0800

    city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -28,13 +28,19 @@ export default {
     return {
       touchStatus: false,
       touchMoveStatus: false,
-      letter: ''
+      letter: '',
+      clientY: '',
+      timer: null
     }
   },
   computed: {
     letters () {
       return Object.keys(this.cities)
     }
+  },
+  updated () {
+    if (this.startY) return
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     HandleLetterClick (e) {
@@ -48,14 +54,19 @@ export default {
       if (this.touchStatus) {
         // 思想：获取A的偏移量，获取touchmove事件的手指在可视区域的位置，减去头部距离
         // ，就是手指距离A的偏移量，除以每个字母的高度，向下取整，就是字母的索引
-        this.touchMoveStatus = true
-        const startY = this.$refs['A'][0].offsetTop
-        const moveY = e.touches[0].clientY - 82
-        const index = Math.floor((moveY - startY) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.letter = this.letters[index]
-          this.$emit('change', this.letter)
-        }
+        if (this.timer) return
+
+        this.timer = setTimeout(() => {
+          this.touchMoveStatus = true
+          const moveY = e.touches[0].clientY - 82
+          const index = Math.floor((moveY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.letter = this.letters[index]
+            this.$emit('change', this.letter)
+          }
+
+          this.timer = null
+        }, 16)
       }
     },
     HandleTouchEnd () {
